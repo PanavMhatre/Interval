958047a68b36eda18e5634c9bce1d2a9ed58aa90
sjalkote <75491816+sjalkote@users.noreply.github.com>
2026-04-18 14:19:01 -0500

    Initial Commit

---
+++ Interval/IntervalApp.swift
@@ -0,0 +1,32 @@
+//
+//  IntervalApp.swift
+//  Interval
+//
+//  Created by Sopan on 4/18/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct IntervalApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
